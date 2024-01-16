b545d4a68fed9ba78c121db187ec61cf8a1e4fa5
kiluchandran <kiluchandran@gmail.com>
2024-01-16 18:57:22 +0530

    Initial commit of tail

---
+++ tail_kilu.py
@@ -0,0 +1,11 @@
+def print_last_lines(file_name, no_of_lines):
+    with open(file_name, "r") as file1:
+        lines = file1.readlines()
+        last_lines = lines[-no_of_lines:]
+        for line in last_lines:
+            print(line.rstrip())
+
+
+file_name = "/home/kilu/states.txt"
+no_of_lines = 10
+print_last_lines(file_name, no_of_lines)
