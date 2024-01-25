e6656eb90ebd3cc9d2732f4abb566932a2847bac
kiluchandran <kiluchandran@gmail.com>
2024-01-25 17:48:44 +0530

    Implemented colored text

--- ls_kilu.py
+++ ls_kilu.py
@@ -1,11 +1,21 @@
 import os
+import sys
+from termcolor import colored, cprint
 
 
 def main():
     path = os.getcwd()  # path of the current working directory
-    dir_list = os.listdir(path)
-    for list in dir_list:
-        print(list, end=" ")
+    lists = os.listdir(path)
+    for item in lists:
+        if os.path.isdir(item):
+            text = colored(item, 'blue')
+        elif os.path.isfile(item):
+            if os.access(item, os.X_OK):
+                text = colored(item, 'green')
+            else:
+                text = colored(item, 'grey')
+        print(text, end=" ")
+
     print()
 
 
