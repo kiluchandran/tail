12fb1d918800ca2ac21b416d4bf34a9d098649d4
kiluchandran <kiluchandran@gmail.com>
2024-01-30 17:10:47 +0530

    Implemented sorting of files as per last modified time

--- ls_kilu.py
+++ ls_kilu.py
@@ -1,11 +1,28 @@
 import os
 import sys
 from termcolor import colored, cprint
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-t", "--time", help="display files as per last updated time", action="store_true")
+args = parser.parse_args()
+
+
+def sorted_by_modified_time(lists):
+    lists.sort(key=os.path.getmtime)
+    lists.reverse()
+    return lists
 
 
 def main():
     path = os.getcwd()  # path of the current working directory
     lists = os.listdir(path)
+    if args.time:
+        lists = sorted_by_modified_time(lists)
+    else:
+        lists.sort()
     for item in lists:
         if os.path.isdir(item):
             text = colored(item, 'blue')
