7d02da38b3aab6e488df9f57783cc579ad91b114
kiluchandran <kiluchandran@gmail.com>
2024-01-31 16:06:57 +0530

    Implemented sorting of files as per last updated time in revere order

--- ls_kilu.py
+++ ls_kilu.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 import os
 import sys
 from termcolor import colored, cprint
@@ -7,6 +10,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-t", "--time", help="display files as per last updated time", action="store_true")
+parser.add_argument(
+    "-r", "--reverse", help="display files as per last updated time in reverse order", action="store_true")
+
 args = parser.parse_args()
 
 
@@ -16,13 +22,21 @@ def sorted_by_modified_time(lists):
     return lists
 
 
+def sorted_by_modified_time_reverse(lists):
+    lists.sort(key=os.path.getmtime)
+    return lists
+
+
 def main():
     path = os.getcwd()  # path of the current working directory
     lists = os.listdir(path)
+    lists.sort()
+    if args.time and args.reverse:
+        lists = sorted_by_modified_time_reverse(lists)
     if args.time:
         lists = sorted_by_modified_time(lists)
-    else:
-        lists.sort()
+    if args.reverse:
+        lists.reverse()
     for item in lists:
         if os.path.isdir(item):
             text = colored(item, 'blue')
